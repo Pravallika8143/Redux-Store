aade9ba25a494bd84dbc289922f5dbaacdb0bed9
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-22 17:14:26 +0530

    counter-props-dispatch

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -7,8 +7,8 @@ function Counter(props){
     return (
         <div className="border border-3 p-3 m-2 border-danger">
             <h1>Counter:{props.count}</h1>
-            <button  class={"me-3"} onClick={()=>{props.dispatch(incCount())}}>Increment</button>
-            <button onClick={()=>{props.dispatch(decCount())}}>Decrement</button>
+            <button  class={"me-3 rounded-3"} onClick={()=>{props.dispatch(incCount())}}>Increment</button>
+            <button  class={"me-3 rounded-3"} onClick={()=>{props.dispatch(decCount())}}>Decrement</button>
         </div>
     )
 }
--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -7,16 +7,16 @@ function Todolist(props){
     return (
         <div className="border border-3 p-3 m-2 border-primary">
             <h1>Todolist</h1>
-            <input type="text" id="d2" class={"me-2 mb-4"} onChange={(e)=>{setNewTodo(e.target.value)}}/>
-            <button onClick={()=>{props.dispatch({type:'ADD_TODO',payload:newtodo})}}>AddTodo</button>
+            <input type="text" id="d2" class={"me-3 mb-4"} onChange={(e)=>{setNewTodo(e.target.value)}}/>
+            <button  class={"me-3 rounded-3"} onClick={()=>{props.dispatch({type:'ADD_TODO',payload:newtodo})}}>AddTodo</button>
             <ul>
                 {props.todos.map((todo) => {
                         return (
                         <li className={todo.status?"text-decoration-line-through":""}>
                             {todo.title}
-                            {todo.status && <button onClick={()=>{props.dispatch({type:'TOGGLE_STATUS',payload:todo.id})}}>Undo</button>}
-                            {!todo.status && <button onClick={()=>{props.dispatch({type:'TOGGLE_STATUS',payload:todo.id})}}>Delete</button>}
-                            <button class={"ms-2 mb-3"}
+                            {todo.status && <button className='ms-3 rounded-3' onClick={()=>{props.dispatch({type:'TOGGLE_STATUS',payload:todo.id})}}>Undo</button>}
+                            {!todo.status && <button className='ms-3 rounded-3' onClick={()=>{props.dispatch({type:'TOGGLE_STATUS',payload:todo.id})}}>Delete</button>}
+                            <button class={"ms-3 mb-3 rounded-3"}
                              onClick={()=>{
                                 props.dispatch({type:"DELETE_TODO",payload:todo.id});
                             }}
