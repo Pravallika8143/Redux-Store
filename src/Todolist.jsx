af7ba1cc0446a061df2a576d77362b5f6746ee7c
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-22 17:15:09 +0530

    todolist-props-dispatch

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import  { connect } from 'react-redux'
+import  { connect } from 'react-redux';
 
 function Todolist(props){
       console.log("todolist component::",props);
