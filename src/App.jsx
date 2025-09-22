130a9d54fbe884ae1eeda8e12ff432bbcc2fc264
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-22 18:12:18 +0530

    store

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Todolist from "./Todolist"
 function App() {
   return (
     <>
-      <div className="border border-3 p-3 m-2 border-success">
+      <div className="border border-2 p-3 m-2 border-success">
         <h1>Redux Store</h1>
         <Counter></Counter>
         <Todolist></Todolist>
