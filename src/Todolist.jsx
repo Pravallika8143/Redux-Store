5d29411f976467dac96dfff31266a184cabd027c
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-10-27 15:53:50 +0530

    todolist

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -8,7 +8,7 @@ function Todolist(props){
         <div className="border border-2 p-3 m-2 border-primary">
             <h1>Todolist</h1>
             <input type="text" id="d2" class={"me-3 mb-4"} placeholder="Enter Todo" onChange={(e)=>{setNewTodo(e.target.value)}}/>
-            <button  class={"me-3 rounded-3"} onClick={()=>{props.dispatch({type:'ADD_TODO',payload:newtodo})}}>AddTodo</button>
+            <button  class={"me-3 rounded-3"} onClick={()=>{props.dispatch({type:'ADD_TODO',payload:{title:newtodo}})}}>AddTodo</button>
             <ul>
                 {props.todos.map((todo) => {
                         return (
